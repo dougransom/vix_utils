4b0fad5fdb3a0987db9c9bb55d67eccb94d46055
Doug Ransom <doug.ransom@gmail.com>
2021-02-16 20:33:22 -0800

    updated test

--- test.ps1
+++ test.ps1
@@ -6,4 +6,7 @@ vixutil -i  -m continuous.csv -t term.csv -c cash.csv --calendar calendar.csv
 
 vixutil -i  -m continuous.parquet -t term.parquet -c cash.parquet --calendar calendar.parquet
 
+vixutil -i  -m continuous.html -t term.html -c cash.html --calendar calendar.html
+
+vixutil -i  -m continuous.xlsx -t term.xlsx  -c cash.xlsx --calendar calendar.xlsx
 cd ..
