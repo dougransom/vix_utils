881dbdb68113915e1ef744d3e24f8c004ce2b5f8
Doug Ransom <doug.ransom@gmail.com>
2023-02-18 08:05:24 -0800

    removed publishing scripts, use flit

--- publish_package_testpypi.ps1
+++
@@ -1,4 +0,0 @@
-#build an sdist (source distribution) package for unimacro
-#no wheel is built, one less thing to worry about
-#use the no-setup option, which requires a 
-flit publish  --format sdist  --no-setup-py  --repository testpypi
