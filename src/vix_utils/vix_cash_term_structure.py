1820e58180a309426c8b9248d34c909c4b788755
Doug Ransom <doug.ransom@gmail.com>
2023-02-18 12:05:03 -0800

    removed duplicated code into read_index_csv

--- src/vix_utils/vix_cash_term_structure.py
+++ src/vix_utils/vix_cash_term_structure.py
@@ -130,12 +130,14 @@ async def get_vix_index_histories(data_directory):
 #        l = await asyncio.gather(*download_coro)
 
 
+    def read_index_csv(fname,col_names,symbol):
+        return pd.read_csv(fname,header=0,names=col_names).assign(Symbol=symbol)
 
-    frames1=(pd.read_csv(fname,header=0,names=["Trade Date","Close"]).assign(Symbol=sym)  for 
+    frames1=(read_index_csv(fname,["Trade Date","Close"],sym)  for 
         fname,sym in zip(index_history_files_with_value_only,symbols_with_value_only))
          
-    frames2=(pd.read_csv(fname,header=0,names=['Trade Date','Open','High','Low','Close']).assign(Symbol=sym) for  
-       fname,sym in zip(index_history_files_with_high_low_close,symbols_with_high_low_close) )
+    frames2=(read_index_csv(fname,['Trade Date','Open','High','Low','Close'],sym) for  
+       fname,sym in zip(index_history_files_with_high_low_close,symbols_with_high_low_close))
     
 
     frames=chain(frames1,frames2)
