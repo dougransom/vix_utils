541552379b8c01839df71bd24616c1d58b2938e7
Doug Ransom <doug.ransom@gmail.com>
2023-02-19 16:37:12 -0800

    checkpoint.

--- src/vix_utils/download_vix_futures.py
+++ src/vix_utils/download_vix_futures.py
@@ -6,6 +6,38 @@ from pathlib import Path
 import itertools
 import vix_futures_term_struture as t
 import datetime as dt
+
+import pandas_market_calendars as mcal
+import calendar as cal
+
+class CBOFuturesDates:
+    def __init__(self):
+        self.cfe_mcal =  mcal.get_calendar('CFE')
+        #valid_days is expensive, so do it once here
+        now=dt.datetime.now()
+        #get info for futures expiring up to January 1 in six years.
+        #no futures currently trade that far out so this should be fine
+        five_years_away=dt.datetime(now.year+6,1,1)
+
+        self.valid_days=self.cfe_mcal.valid_days(start_date='2000-12-20', end_date=five_years_away).to_series();
+
+    def vix_settlement_date_weekly(self, year,  week_number):
+        c = cal.Calendar(cal.SUNDAY)
+
+        m = c.monthdayscalendar(year, 1)
+        md = c.monthdatescalendar(year, 1)
+        # find the first wednesday of the year
+        wednesday_index = 3
+        wednesday_in_first_week = m[0][wednesday_index] != 0
+        first_wednesday = md[0 if wednesday_in_first_week else 1][wednesday_index]
+        settlement_date = first_wednesday + dt.timedelta(weeks=(week_number - 1))
+        #        print(f"Weekly {year} {month} {week_number}  settlement date: {settlement_date}")
+        # no knowns special cases settlment dates for weekly settlement dates as of 2020-08-15
+
+        return settlement_date
+    
+cboe_futures_dates=CBOFuturesDates()
+
 vix_futures_settlement_date_monthly=t.vix_futures_settlement_date_monthly
 
 #sample weeklky url: https://cdn.cboe.com/data/us/futures/market_statistics/historical_data/VX/VX_2022-06-01.csv  for VX+VXT22/M2
@@ -40,15 +72,29 @@ def years_and_months():
     end_year=now.year+2
     return itertools.product( range(2011,end_year),range(1,13))
 
-          
+def years_and_weeks():
+    now = dt.datetime.now()
+    end_year=now.year+2
+    return itertools.product( range(2011,end_year),range(1,53))
+
+def generate_settlement_url(date_str):
+    return  f"https://cdn.cboe.com/data/us/futures/market_statistics/historical_data/VX/VX_{date_str}.csv"
+
 def generate_monthly_url(year,month):
     settlement_date=vix_futures_settlement_date_monthly(year,month)
     settlement_date_str=settlement_date.isoformat()[:10]
-    url=f"https://cdn.cboe.com/data/us/futures/market_statistics/historical_data/VX/VX_{settlement_date_str}.csv"     
+    url=generate_settlement_url(settlement_date_str)    
     return url,settlement_date_str
 
 def generate_monthly_urls():
      return (generate_monthly_url(y,m) for y,m in years_and_months)
+
+def generate_weekly_url(year,week):
+    
+    settlement_date=cboe_futures_dates.vix_settlement_date_weekly(year,week)
+    settlement_date_str=settlement_date.isoformat()[:10]
+    url=generate_settlement_url(settlement_date_str)    
+    return url,settlement_date_str
 
 async def dump_to_file(fn,data):
         try:
@@ -58,69 +104,76 @@ async def dump_to_file(fn,data):
             async with aiofiles.open(fn,mode="w") as f:
                 await f.write(data)
 
-
-
-
 class VXFuturesDownloader:
     historical_data_url="https://www.cboe.com/us/futures/market_statistics/historical_data/"
-    def __init__(self,data_dir):
-
+    def __init__(self,data_dir,session):
+        self.session=session
         #folder to store data files
         self.data_dir = data_dir
         self.futures_data=data_dir/"futures"
-        p=self.futures_data_cache=data_dir/"futures"/"download"
-        p.mkdir(exist_ok=True,parents=True) 
+        self.futures_data_cache=data_dir/"futures"/"download"
+        self.futures_data_cache_weekly=data_dir/"futures"/"download"/"weekly"
+        self.futures_data_cache_monthly=data_dir/"futures"/"download"/"monthly"
+        for p in (self.futures_data_cache_weekly,self.futures_data_cache_monthly):
+            p.mkdir(exist_ok=True,parents=True) 
  
-    async def download_one_future(self,year,month):
+    async def download_one_monthly_future(self,year,month):
         url,expiry=generate_monthly_url(year,month)
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
-                if response.status !=  200:
-                    return 
-                headers=response.headers
-                print(f"Headers {headers}")
-                content_disposition=headers[_cc]
-                cboe_filename= content_disposition.split('"')[-2]   
+        save_path=self.futures_data_cache_monthly
+        return await self.download_one_future(save_path,url,expiry)
+    
+    async def download_one_weekly_future(self,year,week):
+        url,expiry=generate_weekly_url(year,week)
+        save_path=self.futures_data_cache_weekly
+        return await self.download_one_future(save_path,url,expiry)
+    
+    async def download_one_future(self,save_path,url,expiry):
 
-                file_to_save = f"{expiry}.{cboe_filename}"
-                file_with_path=self.futures_data_cache/file_to_save
-                response_data=await response.read()
-                print(f"Text response \n{response_data}")
-                await dump_to_file(file_with_path, response_data)
-                
-    async def download_futures(self):
-        futures_to_download=(self.download_one_future(y,m) for (y,m) in years_and_months())
+        async with self.session.get(url) as response:
+            if response.status !=  200:
+                return 
+            headers=response.headers
+            content_disposition=headers[_cc]
+            cboe_filename= content_disposition.split('"')[-2]   
+
+            file_to_save = f"{expiry}.{cboe_filename}"
+            file_with_path=save_path/file_to_save
+            response_data=await response.read()
+            await dump_to_file(file_with_path, response_data)
+        
+    async def download_monthly_futures(self):
+        futures_to_download=(self.download_one_monthly_future(y,m) for (y,m) in years_and_months())
+        await asyncio.gather(*futures_to_download)
+
+    async def download_weekly_futures(self):
+        futures_to_download=(self.download_one_weekly_future(y,m) for (y,m) in years_and_weeks())
         await asyncio.gather(*futures_to_download)
 
     async def download_history_root(self):
+        async with self.session.get(self.historical_data_url) as response:
+            history_file=self.futures_data_cache/"history.html"
+            history_headers_file=self.futures_data_cache/"history.txt"
+            async with asyncio.TaskGroup() as tg:
+                headers=f"{response.headers}"
+                tg.create_task(dump_to_file(history_headers_file,headers))
+                text=await response.read()
+                tg.create_task(dump_to_file(history_file,text))
 
-
-            async with aiohttp.ClientSession() as session:
-
-                async with session.get(self.historical_data_url) as response:
-                     history_file=self.futures_data_cache/"history.html"
-                     history_headers_file=self.futures_data_cache/"history.txt"
-                     async with asyncio.TaskGroup() as tg:
-                          headers=f"{response.headers}"
-                          tg.create_task(dump_to_file(history_headers_file,headers))
-                          text=await response.read()
-                          tg.create_task(dump_to_file(history_file,text))
-
-                return text     
+        return text     
 
          
 
     
 async def main():
-    user_path = Path(user_data_dir())
-    vixutil_path = user_path / ".vixutil"
-    vixutil_path.mkdir(exist_ok=True)
-    v=VXFuturesDownloader(vixutil_path)
-    await v.download_history_root()
+    async with aiohttp.ClientSession() as session:
+        user_path = Path(user_data_dir())
+        vixutil_path = user_path / ".vixutil"
+        vixutil_path.mkdir(exist_ok=True)
+        v=VXFuturesDownloader(vixutil_path,session)
+      
+        await asyncio.gather(v.download_monthly_futures(),v.download_weekly_futures())
 
-    u=generate_monthly_url(2023,1)
-    await v.download_futures()
-    print(f"U {u}")
+
 
     
 asyncio.run(main())    
