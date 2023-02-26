6cd82d9c3896b6cc0503967f3e54c6f209f71ecb
Doug Ransom <doug.ransom@gmail.com>
2023-02-26 11:14:11 -0800

    checkpoint

--- src/vix_utils/__init__.py
+++ src/vix_utils/__init__.py
@@ -2,3 +2,4 @@
 A library for preparing   VIX Futures and Cash Term Structures for analysis,
 including a  continuous maturity VIX Futures term structure.
 """
+__version__ = '0.0.1'
--- src/vix_utils/play.py
+++ src/vix_utils/play.py
@@ -5,9 +5,10 @@ print(f"df {df}")
 weekly=df[df['Weekly']] 
 monthly=df[df['Weekly'] == False]
 
+from . import __version__
+print(f"Version {version}")
 #monthly.set_index(["Trade Date","MonthTenor"]).unstack()
                   
-
 #from the old way
 #@u.timeit()
 #def pivot_on_contract_maturity(df):
