0c165aa5e3280a975849a1a8cba3dc7a6e2e1389
Doug Ransom <doug.ransom@gmail.com>
2023-02-26 17:45:15 -0800

    checkpoint.

--- examples/a_loadframes.py
+++ examples/a_loadframes.py
@@ -1,8 +1,16 @@
 import pandas as pd
 import vix_utils.download_vix_futures as v
 import vix_utils
+import asyncio as aio
 
 vix_futures_history=v.load_vix_term_structure()
+
+#load it again using asyncio. required if calling from a coroutine. 
+
+async def do_load():
+    return await v.async_load_vix_term_structure()
+
+vix_futures_history=aio.run(do_load())
 
 print(f"The entire VIX Futures History:\n{vix_futures_history}")
 
@@ -17,11 +25,5 @@ pivoted= monthly.set_index(["Trade Date","MonthTenor"]).unstack()
 pivoted.columns.reorder_levels(order=[1,0])
 pivoted=pivoted[["Close","File"]]
 print(f"The monthlys, with a tenor column index, just a few columns:\n{pivoted}")
-print(f"The columns for the stacked frame above:  {pivoted.columns}")
+print(f"The column index for the stacked frame above:\n{pivoted.columns}")
 
-#monthly.set_index(["Trade Date","MonthTenor"]).unstack()
-                  
-#from the old way
-#@u.timeit()
-#def pivot_on_contract_maturity(df):
-#    return df.reset_index().pivot(columns="Contract Month", index="Trade Date")
