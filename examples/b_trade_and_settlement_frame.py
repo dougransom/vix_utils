e9f91222b3c15756454f586c9968db538bf04edc
Doug Ransom <doug.ransom@gmail.com>
2023-02-28 15:04:41 -0800

    checkpoint

---
+++ examples/b_trade_and_settlement_frame.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+from  vix_utils.vix_futures_term_structure  import vix_futures_trade_dates_and_settlement_dates as trade_and_settle
+
+
+df=trade_and_settle()
+
+print(f"Data frame of Trade and Settlmeent Dates\n{df}\ncolumns index:\n{df.columns}")
