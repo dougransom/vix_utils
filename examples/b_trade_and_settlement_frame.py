499a9c440a5cdeebd52cbbbeb7777f7aed3a8359
Doug Ransom <doug.ransom@gmail.com>
2023-03-03 07:06:00 -0800

    checkpoint

--- examples/b_trade_and_settlement_frame.py
+++ examples/b_trade_and_settlement_frame.py
@@ -1,15 +1,17 @@
 import pandas as pd
 
+from sample_utils import pstars
 from  vix_utils   import \
     vix_futures_trade_dates_and_settlement_dates as trade_and_settle, vix_constant_maturity_weights
 
 
 df=trade_and_settle()
-
+pstars()
 print(f"Data frame of Trade and Settlment Dates\n{df}\ncolumns index:\n{df.columns}")
 weights=vix_constant_maturity_weights(df)
-
+pstars()
 print(f"Constant Maturity Weights{weights} column index {weights.columns}")
-weights_2023=weights.loc['2023']
-print(f"Constant Maturity Weights for 2023 {weights}")
+weights_2023=weights.loc['2023-02']
+pstars()
+print(f"\nConstant Maturity Weights for Feburary 2023\n{weights_2023}")
  
