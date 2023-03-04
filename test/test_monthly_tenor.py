62dd3cb36301177b8f0afc9cdc3d531bb4979c96
Doug Ransom <doug.ransom@gmail.com>
2023-03-04 14:08:31 -0800

    checkpoint

--- test/test_monthly_tenor.py
+++ test/test_monthly_tenor.py
@@ -6,15 +6,16 @@ from  functools import partial
 from . import fiso
  
 
+@pytest.mark.parametrize("date_yy_mm_dd,trade_date_str_list",\
+    [ ((2013,1,15),[ '2013-01-16', '2013-02-13','2013-03-20', '2013-04-17', '2013-05-22', '2013-06-19', '2013-07-17', '2013-08-21']),
+    ((2013,1,16),[ '2013-01-16', '2013-02-13','2013-03-20', '2013-04-17', '2013-05-22', '2013-06-19', '2013-07-17', '2013-08-21']) ] )      
 
-def test_jan_15_2013():
-    yy=2013
-    mm=1
-    dd=15
+def test_settlement_from_trade_date(date_yy_mm_dd,trade_date_str_list):
+ 
 
-    settlements_from_tenor=partial(vix_futures_settlement_date_from_trade_date,yy,mm,dd)
+    settlements_from_tenor=partial(vix_futures_settlement_date_from_trade_date,*date_yy_mm_dd)
 
-    dates = [fiso(strdate) for strdate in [ '2013-01-16', '2013-02-13','2013-03-20', '2013-04-17', '2013-05-22', '2013-06-19', '2013-07-17', '2013-08-21']]
+    dates = [fiso(strdate) for strdate in trade_date_str_list]
     for tenor, expected in zip(range(1,1+len(dates)),dates):
         settlement=settlements_from_tenor(tenor)
         assert(settlement==expected)
