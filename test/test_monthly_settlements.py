c9c0bfb6de01b6863837400e9070e9ce98f3e094
Doug Ransom <doug.ransom@gmail.com>
2024-05-20 15:45:15 -0700

    Test showing failure of settlement date described in https://github.com/dougransom/vix_utils/issues/31

--- test/test_monthly_settlements.py
+++ test/test_monthly_settlements.py
@@ -6,7 +6,7 @@ from . import fiso
 
 
 settlment_test_dates= [((2016,12),'2016-12-21'),  ((2021,1),'2021-01-20') , ((2013,12),'2013-12-18'),
-                       ((2013,1),'2013-01-16')]
+                       ((2013,1),'2013-01-16'),((2024,6), '2024-06-18')]
 
 @pytest.mark.parametrize("yy_mm,expected_date_str",settlment_test_dates)
 def test_settlements(yy_mm,expected_date_str):
