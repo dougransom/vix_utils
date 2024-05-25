e62e8e9123888fa38d9b2a03ad7b22a6ce97e4f3
Doug Ransom <doug.ransom@gmail.com>
2024-05-24 20:52:50 -0700

    Squashed commit of the following:
    https://github.com/dougransom/vix_utils/issues/30
    
    Reworked the alogirthm to create weights that VXX etc.  use.
    
    Note that the weights now use the three front month expirations
    run `vixutil -w w.xlsx` to see the new columns T1W, T2W, T3W
    which weight the front three futures.
    
    commit 59727e07d03d1926000093612701b9c6c561cd85
    Author: Doug Ransom <doug.ransom@gmail.com>
    Date:   Fri May 24 20:34:35 2024 -0700
    
        checkpoint
    
    commit be58bd5a9195c9ae98d24137d4cbeb5bf6a60355
    Author: Doug Ransom <doug.ransom@gmail.com>
    Date:   Fri May 24 20:29:24 2024 -0700
    
        checkpoint
    
    commit 4abda16aa45be98a10af4fc8095fa95a86585c43
    Author: Doug Ransom <doug.ransom@gmail.com>
    Date:   Fri May 24 20:07:52 2024 -0700
    
        removed some output debugging.
    
    commit 218e46596734493ff265b7d54489b28891c02800
    Author: Doug Ransom <doug.ransom@gmail.com>
    Date:   Fri May 24 19:54:39 2024 -0700
    
        checkpoint
    
    commit 586ecea46d1651b74caac849cac912d682ee8271
    Author: Doug Ransom <doug.ransom@gmail.com>
    Date:   Fri May 24 19:39:05 2024 -0700
    
        checkpoint
    
    commit 0777a781dbf8bc70fcdba426bd9e931c99e45d26
    Author: Doug Ransom <doug.ransom@gmail.com>
    Date:   Fri May 24 18:30:25 2024 -0700
    
        checkpoint
    
    commit 9c86a9d29693e3bb15b05e73d362f226dbf2169e
    Author: Doug Ransom <doug.ransom@gmail.com>
    Date:   Fri May 24 16:42:44 2024 -0700
    
        checkpoint.
    
    commit ea396cc32c27627186e501bf45e606a61787de44
    Author: Doug Ransom <doug.ransom@gmail.com>
    Date:   Fri May 24 16:06:53 2024 -0700
    
        Trim the date range looping through to that of interest for
        generating weights.  Mostly useful in debugging (not intended to be a performance enhancement)
    
    commit a775224d9f7994c6dcce4f2c1129befed8680127
    Author: Doug Ransom <doug.ransom@gmail.com>
    Date:   Fri May 24 10:52:55 2024 -0700
    
        checkpoint
    
    commit cff78b9a0fd4701a3544029fb570a0d5d39b2aa3
    Author: Doug Ransom <doug.ransom@gmail.com>
    Date:   Fri May 24 09:30:04 2024 -0700
    
        checkpoint
    
    commit 7420a6e0a1e82d8f4c6d10c6be1e1ae1c54b6b1a
    Author: Doug Ransom <doug.ransom@gmail.com>
    Date:   Fri May 24 09:03:02 2024 -0700
    
        checkpoint
    
    commit 918c0b30003dfbcfa06e403b9a0a06fbd4bee065
    Author: Doug Ransom <doug.ransom@gmail.com>
    Date:   Fri May 24 08:22:52 2024 -0700
    
        checkpoint
    
    commit 482dab747184498b32aea58b083742e92733b02d
    Author: Doug Ransom <doug.ransom@gmail.com>
    Date:   Thu May 23 09:22:14 2024 -0700
    
        More clearly identify the roll periods.
    
    commit 7956468342780eefe95b88909ea49c662f7b9235
    Author: Doug Ransom <doug.ransom@gmail.com>
    Date:   Wed May 22 20:19:28 2024 -0700
    
        checkpoint.
    
    commit 9987370ac5f642e1270757274b5c2fa1fc3c4823
    Author: Doug Ransom <doug.ransom@gmail.com>
    Date:   Wed May 22 18:01:05 2024 -0700
    
        Added assertion to detect weights out of allowed range.
    
    commit 7d6a99a08252b18da58422aaf9c16a1943b73da9
    Author: Doug Ransom <doug.ransom@gmail.com>
    Date:   Tue May 21 10:08:25 2024 -0700
    
        Test refactoring so we can select smaller sets of CFE open days.
    
    commit 6e216bf2d5182b994400e321d95b9815adffcf0b
    Author: Doug Ransom <doug.ransom@gmail.com>
    Date:   Tue May 21 08:49:23 2024 -0700
    
        Refactored to facilitate testing.
    
    commit e52bbba15da14abffed4d2d8bd9534c7f6a1eb50
    Author: Doug Ransom <doug.ransom@gmail.com>
    Date:   Mon May 20 19:52:48 2024 -0700
    
        Added a test to highlight
        https://github.com/dougransom/vix_utils/issues/30

--- src/vix_utils/vix_futures_dates.py
+++ src/vix_utils/vix_futures_dates.py
@@ -5,16 +5,17 @@ import calendar as cal
 import datetime as dt
 import numpy as np
 import vix_utils.futures_utils as u
-
+from icecream import ic
 import logging as logging
 from .location import data_dir
+from typing import Generator
 _cfe_calendar = mcal.get_calendar('CFE')
 _now = dt.datetime.now()
 
-_years_ahead = 5
-_future_date = dt.datetime(_now.year + _years_ahead, 1, 1)
+_years_ahead : int = 5
+_future_date : dt.datetime = dt.datetime(_now.year + _years_ahead, 1, 1)
 # must be 1 year past _future_date
-_last_valid_cfe_day = dt.datetime(_now.year + _years_ahead + 1, 1, 1)
+_last_valid_cfe_day : dt.datetime = dt.datetime(_now.year + _years_ahead + 1, 1, 1)
 
 # generate a range that is beyond any possible Expirys for vix futures in the data
 
@@ -23,13 +24,14 @@ _first_vix_futures_date_date = "2004-03-26"
 
 # don't use this for the date range.  _valid_cfe_days should go a year past the last trade date
 
-_valid_cfe_days = pd.DatetimeIndex(
+_valid_cfe_days : pd.DatetimeIndex = pd.DatetimeIndex(
     _cfe_calendar.valid_days(_first_vix_futures_date_date, end_date=_last_valid_cfe_day).date).to_series().sort_index()
 
 
 # days = _valid_cfe_days.date
 # days1 = pd.DatetimeIndex(days).to_series()
 
+minus_one_day=pd.DateOffset(-1)
 
 @func.lru_cache(maxsize=None)  # called repeatedly with the same values, so cache the results.
 def vix_futures_expiry_date_monthly(year: int, month: int):
@@ -56,7 +58,7 @@ def vix_futures_expiry_date_monthly(year: int, month: int):
 
 
     """
-    c = cal.Calendar(firstweekday=cal.SUNDAY)
+    c : cal.Calendar = cal.Calendar(firstweekday=cal.SUNDAY)
     next_month = month + 1
 
     # does the option series the future settles on  settle next year?
@@ -65,21 +67,21 @@ def vix_futures_expiry_date_monthly(year: int, month: int):
     next_month = next_month % 12 if options_next_year else next_month
     options_year = year + 1 if options_next_year else year  # keep for debugging
 
-    m = c.monthdayscalendar(options_year, next_month)
-    md = c.monthdatescalendar(options_year, next_month)
+    m : list[list[int]] = c.monthdayscalendar(options_year, next_month)
+    md : list[list[dt.date]] = c.monthdatescalendar(options_year, next_month)
 
     friday_index = -2
     # 2 to index the 3d week, 0 based index for m
     week_index = 2 if m[0][friday_index] != 0 else 3
     # third_friday unused, just for easier debugging to have.
-    third_friday = m[week_index][friday_index]              # keep for debugging
-    option_expiry_date = md[week_index][friday_index]
-    friday_expiration = any(_valid_cfe_days.isin([option_expiry_date]))
+    third_friday : int = m[week_index][friday_index]              # keep for debugging
+    option_expiry_date : dt.date = md[week_index][friday_index]
+    friday_expiration : bool = any(_valid_cfe_days.isin([option_expiry_date]))
     if not friday_expiration:
         # the preceding day will be the option expiry date
         option_expiry_date = option_expiry_date - dt.timedelta(days=1)
 
-    futures_expiry_date = option_expiry_date - dt.timedelta(days=30)
+    futures_expiry_date : dt.date = option_expiry_date - dt.timedelta(days=30)
     # also check for a holiday on the 30 days before the 3d friday
     if friday_expiration and not any(_valid_cfe_days.isin([futures_expiry_date])):
         futures_expiry_date = futures_expiry_date - dt.timedelta(days=1)
@@ -108,9 +110,11 @@ def vix_futures_expiry_date_from_trade_date(year, month, day, tenor):
     return vix_futures_expiry_date_monthly(year_of_expiry, month_of_expiry)
 
 
-def vix_constant_maturity_weights(vix_calendar):
+def vix_constant_maturity_weights(vix_calendar : pd.DataFrame, start_date : str|pd.Timestamp=None, end_date : str|pd.Timestamp=None) -> pd.DataFrame:
     """
     :param vix_calendar:  the DataFrame returned by  vix_futures_trade_dates_and_expiry_dates
+    :param date_range: A data range to limit the weights, useful for testing or only need weights for a limited range.
+
     :return: a DataFrame containting the weights required to interpolate between the tenors of trading tenors of
     Vix Futures to have a term structure of constant maturity in months.
 
@@ -144,45 +148,208 @@ def vix_constant_maturity_weights(vix_calendar):
 
     # create a map from Expirys to the previous Expirys
     # this is done by looking at month 2 settlment dates, and finding the month 1 Expiry
+    ic.disable()
+    start_roll_col : str = "Start Roll"
+    end_roll_col : str = "End Roll"
 
-    start_roll_front_month = "Start Roll Front Month"
     sd = "Expiry"
     rptd = "Roll Period Trade Days"
     rpcd = "Roll Period Calendar Days"
+    rrptd = "Remaining Roll Period Trade Days"
+    rrpcd = "Remaining Roll Period Calendar Days"
     settle_dates_map = vix_calendar[sd].drop_duplicates().dropna()
-    month_to_prior_month_settlement_map = settle_dates_map.set_index(2)[1]
-    cols_to_copy = {"Settle 1": vix_calendar['Expiry'][1], "Settle 2": vix_calendar['Expiry'][2]}
-    df_foo = pd.DataFrame(index=vix_calendar.index, data=cols_to_copy)
+    month_to_prior_month_settlement_map_full = settle_dates_map.set_index(2)[1]
+
+    #need to patch the March  2024 settlement  date in.
+    month_to_prior_month_settlement_map_full['2004-04-21']='2004-03-16'
+    month_to_prior_month_settlement_map_full.sort_index(inplace=True)
+
+
+    #avoid looping through months with no data, to facillitate easier debugging.
+    #we need the map from the date of the first trade in the range, to month after.
+    cols_to_copy = {f"Settle {i}": vix_calendar['Expiry'][i] for i in range(1,4)}
+
+    df_foo = pd.DataFrame(index=vix_calendar.index,data=cols_to_copy)
+    if start_date or end_date:
+        #we know that we don't need to look more than 30 days in the future, so limit to 70 days
+        # (to avoid any boundry/edge cases) 
+        # to trip the settlement map.
+        last_trade_of_interest=vix_calendar[start_date:end_date].index[-1]
+
+        date_range=slice(start_date,last_trade_of_interest+pd.DateOffset(70))
+        
+        month_to_prior_month_settlement_map = month_to_prior_month_settlement_map_full.loc[date_range]
+        df_foo=df_foo[start_date:end_date] 
+    else:
+        month_to_prior_month_settlement_map=month_to_prior_month_settlement_map_full
+
+
+
+
+    
 
     df_foo[rptd] = -100001  # just a nonsense number we can identify
 
     df_foo[rptd] = df_foo[rptd].astype(int)
 
+
+    df_foo[rpcd] = -999990  # another nonsense number we can identify
+    df_foo[rpcd] = df_foo[rpcd].astype(int)  # another nonsense number we can identify
+
     # add the start of roll date for front month
 
-    df_foo[start_roll_front_month] = np.nan
-    for second_month_settlement in month_to_prior_month_settlement_map.index:
-        front_month_settlement=month_to_prior_month_settlement_map[second_month_settlement]
-        start_roll = front_month_settlement
-        selected = vix_calendar[sd][1] == front_month_settlement
-        df_foo.loc[selected, start_roll_front_month] = start_roll
-      
-        calendar_day_day_before_settlement=second_month_settlement + pd.DateOffset(-1)
+    df_foo[start_roll_col] = np.nan
 
-        #roll_period_trade_days is dt in https://www.spglobal.com/spdji/en/documents/methodologies/methodology-sp-vix-futures-indices.pdf page 5
-        roll_period_trade_days = cfe_exchange_open_days(front_month_settlement, calendar_day_day_before_settlement) 
-        df_foo.loc[selected, rptd] = roll_period_trade_days
+    #useful for debuggin.    
+    df_foo['Day of Week']=df_foo.index.day_of_week
+    df_foo['Day Name']=df_foo.index.day_name()
+    df_foo["First Component"]=1
+    df_foo["Second Component"]=2
+    def roll_periods() -> Generator:
+        """Generator, returns roll periods as tuples start_roll_date, front_month_settlement, end_roll_date-1 day, end_roll_date"""
 
-    df_foo[start_roll_front_month] = pd.to_datetime(df_foo[start_roll_front_month])
-    df_foo[rpcd] = vix_calendar[sd][1] - df_foo[start_roll_front_month]
+  
+        for second_month_settlement in month_to_prior_month_settlement_map.index:
+            front_month_settlement=month_to_prior_month_settlement_map[second_month_settlement]
+            #https://www.spglobal.com/spdji/en/documents/methodologies/methodology-sp-vix-futures-indices.pdf page 5
+            #the Roll Period starts after the
+            #close on the Tuesday prior to the monthly Chicago Board Options Exchange (Cboe) VIX Futures 
+            #Settlement Date
+            #a little ambigous if a tuesday is a holiday.
+            day_before_front_month_settlement = front_month_settlement + minus_one_day
+            
+            #days of weeks start at 0 for Monday
+
+            #it is going to be negative, because we are looking for the days until the preceeding tuesday
+            days_until_tuesday_front_month = 1-front_month_settlement.day_of_week
+            
+
+            start_roll_date : pd.Timestamp = front_month_settlement + pd.DateOffset(days_until_tuesday_front_month)
+
+   
+            #it is going to be negative, because we are looking for the days until the preceeding tuesday
+
+            days_until_tuesday_second_month = 1-second_month_settlement.day_of_week  
+            end_roll : pd.Timestamp =  second_month_settlement + pd.DateOffset(days_until_tuesday_second_month)
+            day_before_end_roll : pd.Timestamp = end_roll+minus_one_day
+
+            yield (start_roll_date,front_month_settlement,day_before_end_roll,end_roll)
+
+    def ammend_df():
+        for (start_roll_date,front_month_settlement,day_before_end_roll,end_roll) in roll_periods():
+                    
+            this_row_period_slice=slice(start_roll_date,day_before_end_roll)
+            ic(this_row_period_slice)
+            df_foo.loc[this_row_period_slice,start_roll_col] = start_roll_date
+            df_foo.loc[this_row_period_slice,end_roll_col] = end_roll
+
+            #roll_period_trade_days is dt in https://www.spglobal.com/spdji/en/documents/methodologies/methodology-sp-vix-futures-indices.pdf page 5
+            #includes first day of the roll period, but excludes the last.
+
+            # After the close on the Tuesday, corresponding to the start of the Roll Period, all of the weight is allocated 
+            # to the shorter-term (i.e., mth month) contract. Then on each subsequent business day a fraction of the mth
+            # month VIX futures holding is sold and an equal notional amount of the longer-term (n
+            # th month) VIX futures
+            # is bought. The fraction, or quantity, is proportional to the number of mth month VIX futures contracts as of 
+            # the previous index roll day, and inversely proportional to the length of the current Roll Period. In this way 
+            # the initial position in the mth month contract is progressively moved to the n
+            # th month one over the course 
+            # of the month, until the following Roll Period starts when the old n
+            # th month VIX futures contract becomes 
+            # the new mth month VIX futures contract and gets sold every day afterward as the process begins again.
+
+            #the above is slightly weird, because the expiring future still trades on the wednesday, though it no weight.
+              
+            #for the  front month settlement day, and the start of the roll period day, we use the second and third month to compute weightings
+
+            df_foo.loc[start_roll_date:front_month_settlement,'First Component']=2
+            df_foo.loc[start_roll_date:front_month_settlement,'Second Component']=3
+
+            roll_period_trade_days = cfe_exchange_open_days(start_roll_date, day_before_end_roll) 
+            
+
+
+            ic(roll_period_trade_days)
+            #use the same methoology for roll period calendar days for choosing the start and end dates.
+            roll_period_calendar_days = int( (day_before_end_roll - start_roll_date)/np.timedelta64(1,'D') )
+
+
+
+            df_foo.loc[this_row_period_slice, rptd] = roll_period_trade_days
+            df_foo.loc[this_row_period_slice, rpcd] = roll_period_calendar_days
+
+    ammend_df()
+    df_foo[start_roll_col] = pd.to_datetime(df_foo[start_roll_col])
     tenor_tds = "Tenor_Trade_Days"
     ten_caldays = "Tenor_Days"
 
     fmw = "Front Month Weight"
     smw = "Next Month Weight"
     trade_days_to_settle = df_foo[tenor_tds] = vix_calendar[tenor_tds][1]
-    df_foo[fmw] = front_month_weight = trade_days_to_settle / df_foo[rptd]
-    df_foo[smw] = -1 * front_month_weight + 1
+
+    #The total number of business days within a Roll Period beginning with, and including, the 
+    #following business day and ending with, but excluding, the following Cboe VIX Futures 
+    #Settlement Date. The number of business days includes a new holiday introduced intra-month 
+    #up to the business day proceeding such a holiday
+
+
+    df_foo["Trade_Day_Plus_1"]=df_foo.index + pd.DateOffset(1)
+    df_foo["End_Roll_Minus_1"]= df_foo[end_roll_col] + minus_one_day
+    df_foo['Settle_1_Minus_1']=df_foo["Settle 1"] + minus_one_day
+
+    def remaining_roll_period_trade_days_on_row(row):
+        return cfe_exchange_open_days(row["Trade_Day_Plus_1"],row['End_Roll_Minus_1'])
+
+    remaining_roll_period_trade_days : pd.DataFrame    
+    df_foo[rrptd]=remaining_roll_period_trade_days=df_foo.apply(remaining_roll_period_trade_days_on_row,axis=1, result_type='expand')
+
+    def remaining_roll_period_calendar_days_on_row(row):
+        return int((row['End_Roll_Minus_1']-row.name)/np.timedelta64(1,'D'))
+
+
+    remaining_roll_period_trade_days : pd.DataFrame
+    df_foo[rrpcd] = remaining_roll_period_calendar_days=df_foo.apply(remaining_roll_period_calendar_days_on_row,axis=1,result_type='expand')
+
+    front_month_weights : pd.Series
+    next_month_weights : pd.Series
+
+
+    df_foo[fmw] = front_month_weights = remaining_roll_period_trade_days / df_foo[rptd]
+    df_foo[smw] = next_month_weights  = -1 * front_month_weights + 1
+
+
+
+
+    #identify problematic weights. 
+    #should never happen if the code is correct.
+    front_month_weights_too_big_sel = front_month_weights >1
+    front_month_weights_too_small_sel = front_month_weights<0
+ 
+
+    if front_month_weights_too_big_sel.any():
+        front_month_weights_too_big=front_month_weights[front_month_weights_too_big_sel]
+        ic(front_month_weights_too_big)
+
+    if front_month_weights_too_small_sel.any():
+        front_month_weights_too_small=front_month_weights[front_month_weights_too_small_sel]
+        ic(front_month_weights_too_small)
+
+    assert front_month_weights.max() <= 1
+    assert front_month_weights.min() >= 0
+    assert ((front_month_weights+next_month_weights)==1).all()          #they better add up to one
+
+    tenor_1_weight = (df_foo["First Component"]==1)*front_month_weights  
+    tenor_2_weight = (df_foo["First Component"]==2) * front_month_weights +\
+                     (df_foo["Second Component"] == 2) * next_month_weights
+    tenor_3_weight = (df_foo["Second Component"] == 3) * next_month_weights
+
+    assert ((tenor_1_weight+tenor_2_weight+tenor_3_weight)==1).all()   #better add up to one!
+
+    df_foo["T1W"]=tenor_1_weight
+    df_foo["T2W"]=tenor_2_weight
+    df_foo["T3W"]=tenor_3_weight
+
+
     ttr = "Temp Trade Date"
     df_foo[ttr] = df_foo.index.to_series()
     #    temp_tdts="Temporary Tenor_Days"
@@ -197,7 +364,7 @@ def vix_constant_maturity_weights(vix_calendar):
             row_roll_period_trade_days = row[rptd]  # trade_days_to_settle[trade_date]
             trade_date_loc = trade_days_to_settle.index.get_loc(trade_date)
             trade_date_loc_end_of_roll = trade_date_loc + row_roll_period_trade_days
-            trade_date_loc_end_of_roll_capped = pd.nan if trade_date_loc_end_of_roll > ll else \
+            trade_date_loc_end_of_roll_capped = np.nan if trade_date_loc_end_of_roll > ll else \
                 trade_date_loc_end_of_roll
             trade_date_end_of_roll = df_foo.iloc[trade_date_loc_end_of_roll_capped].at[ttr]
             return trade_date_end_of_roll
@@ -223,6 +390,17 @@ def vix_constant_maturity_weights(vix_calendar):
 def pivot_on_contract_maturity(df):
     return df.reset_index().pivot(columns="Contract Month", index="Trade Date")
 
+def cfe_exchange_open_dates(selection_slice : slice = None)  -> pd.DatetimeIndex:
+    """
+    Return the dates, sorted, that the CFE is open, based on 
+    partial string indexing (https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#partial-string-indexing). 
+
+    Pass in a slice object that can select from a DateTimeIndex.  like slice('2024-04','2024-04') for April 2024, or slice(None,'2024-04') for
+    all dates until and including April 2024.
+    Pass in both Dates unless you want all dates before or all dates after the partial string index supplied.   
+    
+    """
+    return _valid_cfe_days.loc[selection_slice if selection_slice else slice(None)]
 
 def cfe_exchange_open_days(start_date, end_date):
     exchange_open_days = _valid_cfe_days.loc[start_date:end_date]
@@ -232,8 +410,9 @@ def remaining_cfe_exchange_open_days(start_date,end_date):
     next_day=start_date + pd.DateOffset(1)
     return cfe_exchange_open_days(next_day,end_date)
 
+
 @u.timeit()
-def vix_futures_trade_dates_and_expiry_dates(number_of_futures_maturities=9):
+def vix_futures_trade_dates_and_expiry_dates(number_of_futures_maturities:int = 9) -> pd.DataFrame:
     f"""
     :param number_of_futures_maturities:
         :return:  a data frame with an index of trade date and maturity (in months) and a value of the Expiry.  
@@ -243,7 +422,24 @@ def vix_futures_trade_dates_and_expiry_dates(number_of_futures_maturities=9):
         """
     # by trial and error, this gives us the day
     # careful to select the portion of the valid dates before _future_date
-    trade_dates = _valid_cfe_days[:_future_date]
+    trade_dates :pd.DatetimeIndex = _valid_cfe_days[:_future_date]
+    return _vix_futures_trade_dates_and_expiry_dates_for_dates(trade_dates,number_of_futures_maturities)
+
+@u.timeit()
+def _vix_futures_trade_dates_and_expiry_dates_for_dates(trade_dates : pd.DatetimeIndex,number_of_futures_maturities:int = 9) -> pd.DataFrame:
+    f"""
+    :param trade_dates:  a subset of dates from _valid_cfe_days
+    :param number_of_futures_maturities:
+        :return:  a data frame with an index of trade date and maturity (in months) and a value of the Expiry.  
+                   We refer to a DataFrame in this format as a wide vix calendar or wide settlement calendar..
+                   The dates will include all past dates which the VIX futures have traded, and future dates until
+                    {_future_date}.
+
+        Library consumers should prefer vix_futures_trade_dates_and_expiry_dates.  
+
+        """
+    # by trial and error, this gives us the day
+    # careful to select the portion of the valid dates before _future_date
     ii = pd.Index(trade_dates, name="Trade Date")
 
     @u.timeit()
---
+++ test/test_constant_maturity_weights.py
@@ -0,0 +1,65 @@
+import pytest
+import vix_utils as vu
+from vix_utils.vix_futures_dates import vix_constant_maturity_weights,_vix_futures_trade_dates_and_expiry_dates_for_dates
+
+import pandas as pd
+pd.set_option('display.max_rows',5)
+pd.set_option('display.max_columns',None)
+
+#vix_dates=vu.vix_futures_dates.vix_futures_trade_dates_and_expiry_dates()
+
+#weights=vix_constant_maturity_weights(vix_dates)
+
+#abc=weights.loc["2024-04-15" : "2024-04-30"]
+#print(f"weights:\n{abc}\n")
+
+@pytest.fixture 
+def vix_dates():
+    return vu.vix_futures_dates.vix_futures_trade_dates_and_expiry_dates()
+
+def test_small_date_range():
+   with pd.option_context('display.max_rows',None,'display.max_columns',None):
+    vix_calendar=vu.vix_futures_dates.cfe_exchange_open_dates()
+    test_dates=('2024-03-15','2024-04-30')
+    dates_for_weights   = _vix_futures_trade_dates_and_expiry_dates_for_dates(vix_calendar)
+
+    print(f"dates testing in april:\n{test_dates}\ndates_for_weights:\n{dates_for_weights}\n")
+
+    weights=vix_constant_maturity_weights(dates_for_weights,*test_dates)
+    print(f"Weights:\n{weights}")
+
+
+
+@pytest.fixture
+def m1m2_weights(vix_dates):
+    return vix_constant_maturity_weights(vix_dates)
+
+@pytest.mark.parametrize("column",["Front Month Weight","Next Month Weight"])
+def test_weights_in_range(m1m2_weights : pd.DataFrame,column):
+    ''' Verify the weights of two futures to produce a weighted maturity are in the range [0,1]'''
+
+    #note that with floating point it is possible one day to experince rounding errors that would make a weight a hair outside of 
+    #the range [0,1] in which case this test might have to be tweaked.
+    negative_weights_sel : pd.DataFrame = m1m2_weights[column] < 0
+    weights_gt_1_sel : pd.DataFrame =  m1m2_weights[column] > 1.0
+
+
+    weights_out_of_range=m1m2_weights[negative_weights_sel|weights_gt_1_sel]
+    
+    with pd.option_context('display.max_rows',None,'display.max_columns',None):
+        if not weights_out_of_range.empty:
+             print(f"test_weights_in_range{column}\n:{weights_out_of_range}")
+
+    assert weights_out_of_range.empty, f"{column} should be between 0 and 1:  {weights_out_of_range}"
+
+def test_weights_sum_to_one(m1m2_weights : pd.DataFrame):
+    ''' Verify the weights of two futures to produce a weighted maturity sum to one'''
+    
+    
+    #note that with floating point it is possible one day to experince rounding errors that would make a weight a hair outside of 
+    #the range [0,1] in which case this test might have to be tweaked.
+    weights_sum = m1m2_weights['Front Month Weight'] + m1m2_weights['Next Month Weight']
+    rows_not_summing_to_one = m1m2_weights[ weights_sum !=  1.0]
+    count_rows=rows_not_summing_to_one.shape[0]
+    with pd.option_context('display.max_rows',None,'display.max_columns',None):
+        assert rows_not_summing_to_one.empty, f"The sum of weights of two futures should be one.  {count_rows} instances of failure:\n{rows_not_summing_to_one} "
